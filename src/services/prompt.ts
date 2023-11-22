300f0ba4fb03841a45930d19fdaacd9b17cb983b
Efen Stakes <efenstakes101@gmail.com>
2023-11-22 13:16:04 +0300

    add prompt service

---
+++ src/services/prompt.ts
@@ -0,0 +1,29 @@
+import IPromptResponse from "../models/prompt";
+
+const serverURL: string = 'http://localhost:9090/prompts' // 'https://xqwbyqql42.execute-api.us-west-1.amazonaws.com/prompts';
+
+
+// gets the reply for the given prompt from yoda ai
+export const getPrompt = async ({ prompt }: { prompt: string }): Promise<IPromptResponse | null> => {
+    
+    try {
+    
+        const request = await fetch(
+            serverURL,
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ prompt, })
+            }
+        )
+    
+        const response = await request.json()
+
+        return response
+    } catch (error) {
+        
+        return null
+    }
+}
